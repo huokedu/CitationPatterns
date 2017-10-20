10424e9dce856b61c31cc74fdfbfede46099514f
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-20 21:54:44 +0200

    crawling all arxiv links, printing memstats, fixed blocking mux

--- src/crawl/arxiv.go
+++ src/crawl/arxiv.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
+	"net/http"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
 
+	"github.com/PuerkitoBio/fetchbot"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/jinzhu/configor"
 )
 
@@ -16,23 +25,114 @@ var config = struct {
 }{}
 
 var (
-	url = "http://arxiv.org/list/cs.{}/{}{}?show=1000"
+	url      = "http://arxiv.org/list/cs.{}/{}{}?show=1000"
+	memStats = flag.Duration("memstats", 0, "display memory statistics at a given interval")
 )
 
+func runMemStats(f *fetchbot.Fetcher, tick time.Duration) {
+	var mu sync.Mutex
+	var di *fetchbot.DebugInfo
+
+	// Start goroutine to collect fetchbot debug info
+	go func() {
+		for v := range f.Debug() {
+			mu.Lock()
+			di = v
+			mu.Unlock()
+		}
+	}()
+	// Start ticker goroutine to print mem stats at regular intervals
+	go func() {
+		c := time.Tick(tick)
+		for _ = range c {
+			mu.Lock()
+			printMemStats(di)
+			mu.Unlock()
+		}
+	}()
+}
+
+func printMemStats(di *fetchbot.DebugInfo) {
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+	buf := bytes.NewBuffer(nil)
+	buf.WriteString(strings.Repeat("=", 72) + "\n")
+	buf.WriteString("Memory Profile:\n")
+	buf.WriteString(fmt.Sprintf("\tAlloc: %d Kb\n", mem.Alloc/1024))
+	buf.WriteString(fmt.Sprintf("\tTotalAlloc: %d Kb\n", mem.TotalAlloc/1024))
+	buf.WriteString(fmt.Sprintf("\tNumGC: %d\n", mem.NumGC))
+	buf.WriteString(fmt.Sprintf("\tGoroutines: %d\n", runtime.NumGoroutine()))
+	if di != nil {
+		buf.WriteString(fmt.Sprintf("\tNumHosts: %d\n", di.NumHosts))
+	}
+	buf.WriteString(strings.Repeat("=", 72))
+	fmt.Println(buf.String())
+}
+
 func main() {
+	flag.Parse()
+
+	// Create the muxer
+	mux := fetchbot.NewMux()
+
+	// Handle all errors the same
+	mux.HandleErrors(fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
+		fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+	}))
+
+	// Handle GET requests for html responses, to parse the body and enqueue all links as HEAD
+	// requests.
+	mux.Response().Method("GET").ContentType("text/html").Handler(fetchbot.HandlerFunc(
+		func(ctx *fetchbot.Context, res *http.Response, error error) {
+			// Process the body to find the links
+			_, err := goquery.NewDocumentFromResponse(res)
+			if err != nil {
+				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+				return
+			}
+		}))
+
 	configor.Load(&config, "config.yml")
 	//fmt.Printf("config: %#v", config)
 	//test.SlowNCleanTestNeo4j(config.DB.Name, config.DB.Password, config.DB.Host, config.DB.Port)
 
 	categories := map[string]string{"cv": "CV"}
 
+	h := logHandler(mux)
+	f := fetchbot.New(h)
+
+	// First mem stat print must be right after creating the fetchbot
+	if *memStats > 0 {
+		// Print starting stats
+		printMemStats(nil)
+		// Run at regular intervals
+		runMemStats(f, *memStats)
+		// On exit, print ending stats after a GC
+		defer func() {
+			runtime.GC()
+			printMemStats(nil)
+		}()
+	}
+
+	q := f.Start()
+
 	for _, v := range categories {
 		for i := 1; i <= 17; i++ {
 			for j := 1; j <= 12; j++ {
 				var finalURL = fmt.Sprintf("http://arxiv.org/list/cs.%s/%02d%02d?show=1000", v, i, j)
-				fmt.Print(finalURL + "\n")
+				q.SendStringGet(finalURL)
 			}
 		}
 	}
 
+	q.Close()
+}
+
+func logHandler(wrapped fetchbot.Handler) fetchbot.Handler {
+	return fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
+		if err == nil {
+			fmt.Printf("[%d] %s %s - %s\n", res.StatusCode, ctx.Cmd.Method(), ctx.Cmd.URL(), res.Header.Get("Content-Type"))
+		}
+		wrapped.Handle(ctx, res, err)
+	})
 }
