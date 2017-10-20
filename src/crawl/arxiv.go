4fec62e14bc58118878db4836e4c18fafeb2537c
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-20 23:21:03 +0200

    body parse

--- src/crawl/arxiv.go
+++ src/crawl/arxiv.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"runtime"
 	"strings"
@@ -15,6 +16,8 @@ import (
 	"github.com/jinzhu/configor"
 )
 
+// config represents the datastructure parsed from the "config.yml"-config-file
+// the data will later be used to start the database connection
 var config = struct {
 	DB struct {
 		Name     string
@@ -25,7 +28,8 @@ var config = struct {
 }{}
 
 var (
-	url      = "http://arxiv.org/list/cs.{}/{}{}?show=1000"
+	// url represents the general url template for arxiv
+	url      = "http://arxiv.org/list/cs.%s/%02d%02d?show=1000"
 	memStats = flag.Duration("memstats", 0, "display memory statistics at a given interval")
 )
 
@@ -67,6 +71,17 @@ func printMemStats(di *fetchbot.DebugInfo) {
 	}
 	buf.WriteString(strings.Repeat("=", 72))
 	fmt.Println(buf.String())
+}
+
+func logHandler(wrapped fetchbot.Handler) fetchbot.Handler {
+	return fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
+		if err == nil {
+			fmt.Printf("[%d] %s %s - %s\n", res.StatusCode, ctx.Cmd.Method(), ctx.Cmd.URL(), res.Header.Get("Content-Type"))
+		}
+		body, err := ioutil.ReadAll(res.Body)
+		fmt.Print(string(body[:]))
+		wrapped.Handle(ctx, res, err)
+	})
 }
 
 func main() {
@@ -126,13 +141,4 @@ func main() {
 	}
 
 	q.Close()
-}
-
-func logHandler(wrapped fetchbot.Handler) fetchbot.Handler {
-	return fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
-		if err == nil {
-			fmt.Printf("[%d] %s %s - %s\n", res.StatusCode, ctx.Cmd.Method(), ctx.Cmd.URL(), res.Header.Get("Content-Type"))
-		}
-		wrapped.Handle(ctx, res, err)
-	})
 }
