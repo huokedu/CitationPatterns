e70132234477a82376ba3f47ae66ea41c6649891
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-20 16:48:46 +0200

    correct generation of all urls

--- src/crawl/arxiv.go
+++ src/crawl/arxiv.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/dnk0/CitationPatterns/src/crawl/test"
 	"github.com/jinzhu/configor"
 )
 
@@ -16,8 +15,24 @@ var config = struct {
 	}
 }{}
 
+var (
+	url = "http://arxiv.org/list/cs.{}/{}{}?show=1000"
+)
+
 func main() {
 	configor.Load(&config, "config.yml")
-	fmt.Printf("config: %#v", config)
-	test.SlowNCleanTestNeo4j(config.DB.Name, config.DB.Password, config.DB.Host, config.DB.Port)
+	//fmt.Printf("config: %#v", config)
+	//test.SlowNCleanTestNeo4j(config.DB.Name, config.DB.Password, config.DB.Host, config.DB.Port)
+
+	categories := map[string]string{"cv": "CV"}
+
+	for _, v := range categories {
+		for i := 1; i <= 17; i++ {
+			for j := 1; j <= 12; j++ {
+				var finalURL = fmt.Sprintf("http://arxiv.org/list/cs.%s/%02d%02d?show=1000", v, i, j)
+				fmt.Print(finalURL + "\n")
+			}
+		}
+	}
+
 }
