d9534a7421c3669b9abb7bcf57d2754bf745c566
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-21 10:13:40 +0200

    memstats srcd

--- src/crawl/arxiv.go
+++ src/crawl/arxiv.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"runtime"
-	"strings"
-	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/fetchbot"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/jinzhu/configor"
+	pb "gopkg.in/cheggaaa/pb.v1"
 )
 
 // config represents the datastructure parsed from the "config.yml"-config-file
@@ -32,46 +30,6 @@ var (
 	url      = "http://arxiv.org/list/cs.%s/%02d%02d?show=1000"
 	memStats = flag.Duration("memstats", 0, "display memory statistics at a given interval")
 )
-
-func runMemStats(f *fetchbot.Fetcher, tick time.Duration) {
-	var mu sync.Mutex
-	var di *fetchbot.DebugInfo
-
-	// Start goroutine to collect fetchbot debug info
-	go func() {
-		for v := range f.Debug() {
-			mu.Lock()
-			di = v
-			mu.Unlock()
-		}
-	}()
-	// Start ticker goroutine to print mem stats at regular intervals
-	go func() {
-		c := time.Tick(tick)
-		for _ = range c {
-			mu.Lock()
-			printMemStats(di)
-			mu.Unlock()
-		}
-	}()
-}
-
-func printMemStats(di *fetchbot.DebugInfo) {
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
-	buf := bytes.NewBuffer(nil)
-	buf.WriteString(strings.Repeat("=", 72) + "\n")
-	buf.WriteString("Memory Profile:\n")
-	buf.WriteString(fmt.Sprintf("\tAlloc: %d Kb\n", mem.Alloc/1024))
-	buf.WriteString(fmt.Sprintf("\tTotalAlloc: %d Kb\n", mem.TotalAlloc/1024))
-	buf.WriteString(fmt.Sprintf("\tNumGC: %d\n", mem.NumGC))
-	buf.WriteString(fmt.Sprintf("\tGoroutines: %d\n", runtime.NumGoroutine()))
-	if di != nil {
-		buf.WriteString(fmt.Sprintf("\tNumHosts: %d\n", di.NumHosts))
-	}
-	buf.WriteString(strings.Repeat("=", 72))
-	fmt.Println(buf.String())
-}
 
 func logHandler(wrapped fetchbot.Handler) fetchbot.Handler {
 	return fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
@@ -139,6 +97,13 @@ func main() {
 			}
 		}
 	}
+	count := 100000
+	bar := pb.StartNew(count)
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.FinishPrint("The End!")
 
 	q.Close()
 }
