32ddfa6739d6ecf95139e0d38ded234ae076b191
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-27 13:02:29 +0200

    Style Sidebar
    Styling the sidebar component
    Styling the list component
    Removed List Dependencies from mui

--- src/f_spacial/src/containers/sidebar/results/index.js
+++ src/f_spacial/src/containers/sidebar/results/index.js
@@ -5,71 +5,75 @@ import { connect } from 'react-redux';
 import './stylesheets/Results.css';
 
 // NPM Modules
-import List, { ListItem, ListItemText } from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
-import Face from 'material-ui-icons/Face';
-import File from 'material-ui-icons/InsertDriveFile';
-import Divider from 'material-ui/Divider';
-
+import Grid from 'material-ui/Grid';
+import InsertDriveFile from 'material-ui-icons/InsertDriveFile';
 
 // Assets
 
 // Actions
 
 class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayAuthor = this.displayAuthor.bind(this);
+    this.displayTitle = this.displayTitle.bind(this);
+  }
+
+  displayAuthor(author_name) {
+    let new_author_name = author_name.trim();
+    if(new_author_name.length > 23) {
+      new_author_name = new_author_name.substring(0,20);
+      new_author_name = new_author_name.trim();
+      new_author_name += "...";
+    }
+    return new_author_name;
+  }
+
+  displayTitle(title) {
+    let new_title = title.trim();
+    if(new_title.length > 63) {
+      new_title = new_title.substring(0, 60);
+      new_title = new_title.trim();
+      new_title += "...";
+    }
+    return new_title;
+  }
 
   render() {
     return (
       <div className="ResultsList">
-        <List>
+        <ul className="List">
           {this.props.results.map((res, i) => {
             return (
-              <ListItem button key={i}>
-                <Avatar>
-                  <Face/>
-                </Avatar>
-                <ListItemText primary={res} secondary="Jan 9, 2016" />
-              </ListItem>
+              <Grid container direction={'column'} spacing={0}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <li key={i}>
+                    <Grid container direction={'column'} spacing={0}>
+                      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <div className="ListItem_Header">
+                          <Grid container direction={'row'} spacing={0}>
+                            <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
+                              <InsertDriveFile />
+                            </Grid>
+                            <Grid item xs={9} sm={9} md={9} lg={9} xl={9}>
+                              <div className="ListItem_Author">
+                                {this.displayAuthor(res.author)}
+                              </div>
+                            </Grid>
+                          </Grid>
+                        </div>
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        {this.displayTitle(res.name+res.name)}
+                      </Grid>
+                    </Grid>
+                  </li>
+                </Grid>
+              </Grid>
               );
             })
           }
-          <Divider />
-          {this.props.results.map((res, i) => {
-            return (
-              <ListItem button key={i+this.props.results.length}>
-                <Avatar>
-                  <File/>
-                </Avatar>
-                <ListItemText primary={res} secondary="Jan 9, 2016" />
-              </ListItem>
-              );
-            })
-          }
-          <Divider />
-          {this.props.results.map((res, i) => {
-            return (
-              <ListItem button key={i+this.props.results.length}>
-                <Avatar>
-                  <File/>
-                </Avatar>
-                <ListItemText primary={res} secondary="Jan 9, 2016" />
-              </ListItem>
-              );
-            })
-          }
-          <Divider />
-          {this.props.results.map((res, i) => {
-            return (
-              <ListItem button key={i+this.props.results.length}>
-                <Avatar>
-                  <File/>
-                </Avatar>
-                <ListItemText primary={res} secondary="Jan 9, 2016" />
-              </ListItem>
-              );
-            })
-          }
-        </List>
+        </ul>
       </div>
     );
   }
