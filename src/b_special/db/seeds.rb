0df5905e3c3c6a3daad77e9b58c57bf2a29232ba
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-29 19:03:42 +0100

    Sample seed data

--- src/b_special/db/seeds.rb
+++ src/b_special/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PAPER_TITLES = 10.times.map { Faker::Internet.domain_word }
+
+SPLIT=0.4
+OFFSET = PAPER_TITLES.length * SPLIT
+
+
+PAPER_USERS = {
+  'dennis@example.com' => PAPER_TITLES[0,OFFSET],
+  'simon@example.com' => PAPER_TITLES[OFFSET..-1]
+}
+
+PAPER_USERS.each do |email, papers|
+  user = User.new(email: email, password: 'secret')
+  user.save()
+  papers.each {|paper|
+  paper = Paper.new(title: paper)
+  paper.save()
+  }
+end
