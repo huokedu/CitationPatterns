c75be07caee29971a451a51349c944a97782bc33
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-29 18:56:17 +0100

    added paper model

--- src/b_special/db/schema.rb
+++ src/b_special/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029175048) do
+ActiveRecord::Schema.define(version: 20171029175544) do
+
+  create_table "papers", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
