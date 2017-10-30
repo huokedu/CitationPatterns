948d9f4f093d5857b3ba5ed8fe40c02b6d987972
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-10-30 08:25:51 +0100

    use neo4j hardware id for now

--- src/b_spacial/app/models/paper.rb
+++ src/b_spacial/app/models/paper.rb
@@ -1,5 +1,6 @@
-class Paper 
+class Paper
   include Neo4j::ActiveNode
+  id_property :neo_id
   property :title, type: String
   property :author, type: String
   property :abstract, type: String
--- src/b_spacial/db/neo4j/migrate/20171029215011_populate_id_properties.rb
+++
@@ -1,9 +0,0 @@
-class PopulateIdProperties < Neo4j::Migrations::Base
-  def up
-    populate_id_property :Paper
-  end
-
-  def down
-    raise Neo4j::IrreversibleMigration
-  end
-end
