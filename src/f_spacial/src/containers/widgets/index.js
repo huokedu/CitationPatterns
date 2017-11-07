87120d2add5399a03145714f3fbbd03cc8dc973d
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-11-07 09:32:49 +0100

    Fix rendering Error and Pending container bodies

--- src/f_spacial/src/containers/widgets/index.js
+++ src/f_spacial/src/containers/widgets/index.js
@@ -45,16 +45,14 @@ export default class Widget extends Component {
           widgetToRender = <Results expanded={this.state.expanded} query={this.props.widget.query} data={this.props.widget.data}/>;
           break;
         case WidgetTypeNames.ERROR:
-          widgetToRender = <Errors data={this.props.widget.data}/>;
+          widgetToRender = <Errors expanded={this.state.expanded} query={this.props.widget.query} data={this.props.widget.data}/>;
           break;
         case WidgetTypeNames.PENDING:
           widgetToRender =
-            <div className="card full_widget">
               <div className="pending_body">
                 <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
                 <span className="sr-only">Loading...</span>
-              </div>
-            </div>;
+              </div>;
           break;
         default:
           widgetToRender = <div>Nothing to see</div>
