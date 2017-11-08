a924cbaf4432c17b65795d8e9c9302a8457cef6b
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-11-08 15:32:11 +0100

    Add Seed mockup data

---
+++ src/b_spacial/db/seeds.rb
@@ -0,0 +1,4 @@
+# Erweitern für mehr Standarddatensätze zum kleinen testen
+paper1 = Paper.create(:title => "Neo4j.rb is awesome", :abstract => "nice abstract", :full_text => "lorperjwpgnwopgnpw3gpegnopeqngoöqengoöneqgoöneqoögn")
+paper2 = Paper.create(:title => "everything is good", :abstract => "abstract", :full_text => "abc")
+paper3 = Paper.create(:title => "something is awesome", :abstract => "nice ", :full_text => "wasdlolicantmove")
