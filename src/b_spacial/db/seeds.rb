087a2abb5f71bef10ffb355b83abfd720a5a5e37
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-11-10 16:55:06 +0100

    Add Author creation to seeds

--- src/b_spacial/db/seeds.rb
+++ src/b_spacial/db/seeds.rb
@@ -28,3 +28,23 @@ File.foreach('../data/ANN/paper_outcites.txt').with_index do |line, line_num|
   count += 1
   puts "processed papers: " + count.to_s
 end
+
+# uncomment if you want to parse authors
+count = 0
+File.foreach('../data/ANN/author_ids.txt').with_index do |line, line_num|
+  begin
+    list_line = line.split(/\t/)
+    name = list_line[1]
+    names = name.split(/,/)
+    author = Author.new(
+      :first_name => names[1].gsub("\n",''),
+      :last_name => names[0].gsub("\n",'')
+    )
+    author.save
+    author_to_array = { :id => list_line[0], :uuid => author.uuid }
+    authors_created.push author_to_array
+  rescue => ex
+  end
+  count += 1
+  puts "processed authors: " + count.to_s
+end
