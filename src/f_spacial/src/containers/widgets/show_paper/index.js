c0ec1140e557d74d13d3fd1501b5c6963a7df264
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-11-12 16:42:03 +0100

    Add correct formatted references

--- src/f_spacial/src/containers/widgets/show_paper/index.js
+++ src/f_spacial/src/containers/widgets/show_paper/index.js
@@ -16,22 +16,58 @@ import './stylesheets/ShowPaper.css';
 // Actions
 
 class ShowPaper extends Component {
+
+  formatReference(ref) {
+    let formattedRef = "";
+    const authLen  = ref.authors.length;
+    ref.authors.map((author, index) => {
+      if (index !== 0){
+        formattedRef += " ";
+      }
+      formattedRef += author.author.first_name.charAt(0) + ". " + author.author.last_name;
+      if (!(index + 1 === authLen)){
+        formattedRef += ","
+      }
+      return formattedRef
+    });
+    formattedRef += ": " + ref.published + ', ' + ref.title;
+    return formattedRef;
+  }
+
   render() {
     return (
       <div>
         {this.props.expanded && <div className="results_container">
-          <table>
+          <table className="ref_table">
             <thead>
               <tr>
                 <th>
-                  References
+                  References In:
                 </th>
               </tr>
             </thead>
             <tbody>
-              { this.props.data.references.map((ref, index) => {
+              { this.props.data.references_in.map((ref, index) => {
                   return (<tr role="row" key={this.props.data.result.id + '' + index}>
-                    <td>{ref.published + ', ' + ref.title}</td>
+                    <td>{this.formatReference(ref)}</td>
+                  </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+          <table className="ref_table">
+            <thead>
+              <tr>
+                <th>
+                  References Out:
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              { this.props.data.references_out.map((ref, index) => {
+                  return (<tr role="row" key={this.props.data.result.id + '' + index}>
+                    <td>{this.formatReference(ref)}</td>
                   </tr>
                   )
                 })
