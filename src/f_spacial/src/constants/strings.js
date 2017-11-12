12f646129496b1f7dfa965f421a93997252d1e57
Dennis Kreußel <dennis.kreussel94@gmail.com>
2017-11-12 13:04:46 +0100

    Add example string constant for the empty state

---
+++ src/f_spacial/src/constants/strings.js
@@ -0,0 +1,13 @@
+export const strings = {
+  widgets : {
+    results: {
+      empty_state: {
+        title: "",
+        body: {
+          header: "we aint found shit.",
+          description: "We searched far and wide and couldn't find anything matching your search."          
+        }
+      }
+    }
+  }
+}
