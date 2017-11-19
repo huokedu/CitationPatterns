b7fe3b7c02218c233dda1f6b5269ceace63b577f
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-19 17:33:01 +0100

    Added select-option to the results
    Single articles can be selected or deselected now.
    
    The selection ist stored in the ResultTable-state under selected

--- src/f_spacial/src/containers/widgets/results/resultRow.js
+++ src/f_spacial/src/containers/widgets/results/resultRow.js
@@ -13,18 +13,43 @@ import './stylesheets/Results.css';
 import { QueryActions } from '../../../redux/query';
 
 class ResultRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: true
+    };
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange(event) {
+    this.setState({isSelected: event.target.checked});
+    this.props.onChange(this.props.data.paper.id, event.target.checked);
+  };
+
+
   render() {
     return (
-        <tr
-          className="results_row"
-          onClick={() => {
-            this.props.queryActions.querySingle(this.props.data.paper.id , this.props.data.paper.title);
-          }}
-        >
-          <td>{this.props.data.paper.title}</td>
-          <td>{this.props.data.paper.author}</td>
-        </tr>
-      );
+      <tr className="results_row">
+        <td onClick={() => {
+          this.props.queryActions.querySingle(this.props.data.paper.id , this.props.data.paper.title);
+        }}>
+          {this.props.data.paper.title}
+        </td>
+        <td onClick={() => {
+          this.props.queryActions.querySingle(this.props.data.paper.id , this.props.data.paper.title);
+        }}>
+          {this.props.data.paper.author}
+        </td>
+        <td>
+          <input
+            type="checkbox"
+            name="selected_paper"
+            checked={this.state.isSelected}
+            onChange={this.handleChange}
+          />
+        </td>
+      </tr>
+    );
   }
 }
 
--- src/f_spacial/src/containers/widgets/results/resultsTable.js
+++ src/f_spacial/src/containers/widgets/results/resultsTable.js
@@ -14,6 +14,28 @@ import './stylesheets/Results.css';
 import { QueryActions } from '../../../redux/query';
 
 class ResultsTable extends Component {
+  constructor(props) {
+    super(props);
+    let idArray = this.props.data.resultSet.map((result) => {
+      return result.paper.id
+    });
+    let selected = {};
+    idArray.forEach((id) => {
+      selected[id] = true;
+    });
+
+    this.state = {
+      selectedPapers: selected
+    };
+    this.handleCheck = this.handleCheck.bind(this);
+  };
+
+  handleCheck (id, isChecked) {
+    let newState = Object.assign({}, this.state);
+    newState.selectedPapers[id] = isChecked;
+    this.setState(newState);
+  }
+
   render() {
     return (
       <table>
@@ -21,6 +43,12 @@ class ResultsTable extends Component {
           <tr>
             <th>Title</th>
             <th>Authors</th>
+            <th><input
+              type="checkbox"
+              name="selected_paper"
+              checked={this.state.isSelected}
+              onChange={this.handleChange}
+            /></th>
           </tr>
         </thead>
         <tbody>
@@ -30,10 +58,10 @@ class ResultsTable extends Component {
                 key={i}
                 queried
                 data={result}
+                onChange={this.handleCheck}
               />
             );
-            }))
-          }
+          }))}
         </tbody>
       </table>
     );
